fd9028495824940be7a0c504a5aca8a62ce0742b
Esteban12f <joehv33@gmail.com>
2024-12-05 20:56:54 -0500

    Actualizado final

--- src/App.java
+++ src/App.java
@@ -1,5 +1,13 @@
 public class App {
     public static void main(String[] args) throws Exception {
+        Recursividad rec = new Recursividad();
+        
+        System.out.println(rec.factorial(5));
+        System.out.println(rec.sumaNumerosConsecutivos(5));
+        System.out.println(rec.potencia(3, 3));
+        System.out.println(rec.sumaDigitos(456));
+        System.out.println(rec.fibonacci(8));
+        
         RenombrarDirectorios rD = new RenombrarDirectorios();
         rD.renombrarDirectorios("src/directorios");
     }
--- src/Recursividad.java
+++ src/Recursividad.java
@@ -1,4 +1,33 @@
 public class Recursividad {
+    
+    public int factorial(int n){
+        System.out.println("Calculando el factorial de " + n);
+        if (n == 0 || n == 1){
+            System.out.println("Caso base alcanzado, el factorial de" + n + "es = 1");
+            return 1;
+        }
+        int resultado = n*factorial(n - 1);
+        System.out.println("Resultado parcial para " + n + " * factorial("+ (n-1)+") = " + resultado);
+        return resultado;
+    }
+
+    public int sumaNumerosConsecutivos(int n){
+        if(n == 1){
+            return 1;
+        }
+        return n + sumaNumerosConsecutivos(n - 1);
+    }
+
+    public int potencia(int base, int exponente){
+        if (exponente == 0) return 1;
+        return base * potencia(base, exponente - 1);
+    }
+
+    public int sumaDigitos(int n){
+        if (n < 10) return n;
+        return (n % 10) + sumaDigitos(n / 10);
+    }
+
     public int fibonacci(int n){
         if (n == 0) return 0;
         if (n == 1) return 1;
